2589ca51629526374195d2e2816575bab598019c
Peter Szabo <pts@fazekas.hu>
2017-07-24 17:44:48 +0200

    made macOS cross-compilation more consistent

--- c-darwinc64.sh
+++ c-darwinc64.sh
@@ -7,32 +7,32 @@ rm -f *.o empty.bin
 # The image ID is also a digest, and is a computed SHA256 hash of the image configuration object, which contains the digests of the layers that contribute to the image's filesystem definition.
 # REPOSITORY             TAG                 DIGEST                                                                    IMAGE ID            CREATED             SIZE
 # multiarch/crossbuild   latest              sha256:84a53371f554a3b3d321c9d1dfd485b8748ad6f378ab1ebed603fe1ff01f7b4d   846ea4d99d1a        5 months ago        2.99 GB
-   CC="docker run -t -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/o64-clang -mmacosx-version-min=10.5"
-  CXX="docker run -t -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/o64-clang++ -mmacosx-version-min=10.5"
- CCLD="docker run -t -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/o64-clang -mmacosx-version-min=10.5 -Ldarwin_libgcc/x86_64-apple-darwin10/4.9.4 -lSystem -lgcc -lcrt1.10.5.o -nostdlib"
-STRIP="docker run -t -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/x86_64-apple-darwin14-strip"
+  CCC="docker run -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/o64-clang -mmacosx-version-min=10.5 -c"
+ CXXC="docker run -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/o64-clang++ -mmacosx-version-min=10.5 -c"
+ CCLD="docker run -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/o64-clang -mmacosx-version-min=10.5 -Ldarwin_libgcc/x86_64-apple-darwin10/4.9.4 -lSystem -lgcc -lcrt1.10.5.o -nostdlib"
+STRIP="docker run -v $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/x86_64-apple-darwin14-strip"
 test -f darwin_libgcc/x86_64-apple-darwin10/4.9.4/libgcc.a
 
-$CC -O2 -c  \
+$CCC -O2 \
     -ffunction-sections -fdata-sections \
     -W -Wall \
     -I. \
     zall.c
 
-$CC -O2 -c \
+$CCC -O2 \
     -ffunction-sections -fdata-sections \
     -W -Wall -Wno-uninitialized -Wno-sign-compare -Wno-self-assign \
     -I. \
     pngall.c
 
-$CC -O2 -c \
+$CCC -O2 \
     -ffunction-sections -fdata-sections \
     -W -Wall -Wno-uninitialized -Wno-unused-parameter -Wno-sign-compare \
     -Wno-strict-aliasing -fno-strict-aliasing \
     -I. \
     leptonica.c
 
-$CXX -fno-exceptions -fno-rtti -O2 -c \
+$CXXC -fno-exceptions -fno-rtti -O2 \
     -ffunction-sections -fdata-sections \
     -W -Wall \
     -I. \
